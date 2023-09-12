40092344f6b46dd971753ff3e25618a87c4d705e
higorxi <higorgiovane7@gmail.com>
2023-09-12 00:43:02 -0300

    retirada de teste

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,12 @@ import Skills from './components/Skills';
 import Contact from './components/Contact' 
 import Footer from './components/Footer';
 import CourseCarousel from './components/CourseCarousel';
-import Teste from './components/Teste';
-
 
 function App() {
   return (
   <main>
     <Navbar />
     <About />
-    <Teste/>
     <Projects />
     <Skills />
     <CourseCarousel/>
